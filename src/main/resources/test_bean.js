9c27179615882cb9b3c183a47bd88e2180a43475
黄志军 <huangzj@123>
2021-08-20 17:50:04 +0800

    Update test_bean.js

--- src/main/resources/test_bean.js
+++ src/main/resources/test_bean.js
@@ -91,6 +91,7 @@ let num;
     message += "----\n\n"
   }
 
+  that.log(message)
   postToDingTalk(message)
   if ($.isNode() && allMessage) {
     await notify.sendNotify(`${$.name}`, `${allMessage}`)
@@ -777,7 +778,7 @@ function Env(t,e){"undefined"!=typeof process&&JSON.stringify(process.env).index
 
 //我加的函数
 function postToDingTalk(messgae) {
-    const dingtalk = "https://oapi.dingtalk.com/robot/send?access_token=fa87e34729eaa6113fddfa857efebb477dea0a433d6eecfe93b1d3f5e24847b9"
+    const dingtalk = "https://oapi.dingtalk.com/robot/send?access_token=2a8124fdff33639034e415df2aacba8b6befdf12c97a881d83c95ddedaf64518"
 
     const message1 = "" + messgae
     that.log(messgae)
